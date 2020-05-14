678f7bedfc8adf16d123027826755ca000d3b242
Biru4015 <rambirendra12@gmail.com>
2020-05-14 16:44:28 +0530

    [Birendra]Add added. code for zero to zero feet

---
+++ QuantityMeasurement/Conversion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using QuantityMeasurement;
+
+namespace QuantityMeasurement
+{
+    public class Conversion
+    {
+        public double feet;
+
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO QUANTITY MEASUREMENT TTD PROBLEM!!");
+        }
+
+        /// <summary>
+        /// Method to Feet
+        /// </summary>
+        /// <param name="input"> input </param>
+        /// <returns> Calculate Feet </returns>
+        public double Feetmeasure(double input)
+        {
+            return feet = 12 * input;
+        }
+    }
+}
