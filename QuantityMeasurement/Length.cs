4881530e10527e60684b9a91fab1a7df8bfea315
Biru4015 <rambirendra12@gmail.com>
2020-05-15 15:44:12 +0530

    [Birendra]Add added. the method for adding length in inches

--- QuantityMeasurement/Length.cs
+++ QuantityMeasurement/Length.cs
@@ -16,7 +16,8 @@ namespace QuantityMeasurement
         public enum Unit
         {
             FEET,
-            INCH
+            INCH,
+            CENTIMETER
         };
 
         /// <summary>
@@ -83,10 +84,48 @@ namespace QuantityMeasurement
                 case "InchToCentimeter":
                     double inchToCentimeter = givenValue * 2.5;
                     return inchToCentimeter;
+                case "CentimeterToInch":
+                    double centimeterToInch = givenValue / 2.5;
+                    return centimeterToInch;
                 default:
                     Console.WriteLine("Invalid Conversion");
                     return 0;
             }
+        }
+
+        /// <summary>
+        /// This method  is creating for adding two length in inches
+        /// </summary>
+        /// <param name="unitOne"></param>
+        /// <param name="valueOne"></param>
+        /// <param name="unitTwo"></param>
+        /// <param name="valueTwo"></param>
+        /// <returns></returns>
+        public double AddTwoLenghtsInInch(Unit unitOne, double valueOne, Unit unitTwo, double valueTwo)
+        {
+            double firstValueInInch = valueOne;
+            double secondValueInInch = valueTwo;
+            //
+            if (unitOne == Unit.INCH && unitTwo == Unit.INCH)
+                return firstValueInInch + secondValueInInch;
+            if (unitOne == Unit.FEET)
+            {
+                firstValueInInch = LengthConversion("FeetToInch", valueOne);
+            }
+            else if (unitOne == Unit.CENTIMETER)
+            {
+                firstValueInInch = LengthConversion("CentimeterToInch", valueOne);
+            }
+
+            if (unitTwo == Unit.FEET)
+            {
+                secondValueInInch = LengthConversion("FeetToInch", valueOne);
+            }
+            else if (unitTwo == Unit.CENTIMETER)
+            {
+                secondValueInInch = LengthConversion("CentimeterToInch", valueOne);
+            }
+            return firstValueInInch + secondValueInInch;
         }
 
         /// <summary>
