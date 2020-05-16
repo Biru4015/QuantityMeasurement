6a8d09002e56eaf0269a3c40f02d398066e52315
Biru4015 <rambirendra12@gmail.com>
2020-05-16 16:18:00 +0530

    [Birendra]Add added. class for tempreture conversion

---
+++ QuantityMeasurement/Tempreture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+    public class Temperature
+    {
+        public enum Unit
+        {
+            FAHRENHIET,
+            CELSIUS,
+        };
+
+        /// <summary>
+        /// Temperature Conversion
+        /// Fahrenhite to Celsius and Celsius to fahrenhite
+        /// </summary>
+        /// <param name="conversion"></param>
+        /// <param name="givenValue"></param>
+        /// <returns></returns>
+        public double TemperatureConversion(string conversion, double givenValue)
+        {
+            switch (conversion)
+            {
+                case "FahrenhiteToCelsius":
+                    return (givenValue - 32) * 5 / 9;
+                case "CelsiusToFahrenhiet":
+                    return (givenValue * 9 / 5) + 32;
+                default:
+                    Console.WriteLine("Invalid Conversion type!");
+                    break;
+            }
+            return 0;
+        }
+    }
+}
