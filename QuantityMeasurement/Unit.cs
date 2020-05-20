2ee0f2187b3ff94941af09d75b4e60526af14370
Biru4015 <rambirendra12@gmail.com>
2020-05-20 20:48:57 +0530

    [Birendra]Refactor refactored. the code using Unit class and Quantity class

---
+++ QuantityMeasurement/Unit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+    public class Unit
+    {
+        public static readonly Unit FEET = new Unit(12.0);
+        public static readonly Unit INCH = new Unit(1.0);
+        public static readonly Unit CENTIMETER = new Unit(0.4);
+        public static readonly Unit YARD = new Unit(36.0);
+        public static readonly Unit LITRE = new Unit(1.0);
+        public static readonly Unit GALLON = new Unit(3.78);
+        public static readonly Unit MILILITRE = new Unit(0.001);
+        public static readonly Unit KILOGRAM = new Unit(1.0);
+        public static readonly Unit GRAM = new Unit(0.001);
+        public static readonly Unit TONNE = new Unit(1000);
+        public static readonly Unit FAHRENHIET = new Unit(1.0);
+        public static readonly Unit CELCIUS = new Unit(2.12);
+
+        public static IEnumerable<Unit> Values
+        {
+            get
+            {
+                yield return FEET;
+                yield return INCH;
+                yield return CENTIMETER;
+                yield return YARD;
+                yield return LITRE;
+                yield return GALLON;
+                yield return MILILITRE;
+                yield return KILOGRAM;
+                yield return GRAM;
+                yield return TONNE;
+                yield return FAHRENHIET;
+                yield return CELCIUS;
+            }
+        }
+        private double ConversionFactor; 
+
+        private Unit(double baseUnitConvertor)
+            => (ConversionFactor) = (baseUnitConvertor);
+
+        public double GetConvertedValue(double value)
+        {
+            return this.ConversionFactor * value;
+        }
+    }
+}
